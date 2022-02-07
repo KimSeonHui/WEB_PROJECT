52bd63c56f11efd44e125a6c037ee576216d637b
KimSeonHui <zero9657@naver.com>
2022-02-07 18:56:35 +0900

    create wirte button

--- Web-Project-react/client/src/component/MainPage.js
+++ Web-Project-react/client/src/component/MainPage.js
@@ -1,16 +1,37 @@
-import { Box, Typography, Divider, Container   } from '@mui/material';
+import { Box, Typography, Divider, Container, Button } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
 import Table from "./MainTable";
+
+const theme = createTheme({
+    palette: {
+      primary: {
+          main : '#003964',
+      }
+    },
+  });
 
 function Main() {
     return <Container maxWidth="xl">
         <Box sx={{width : "100%", p : '20px'}}>
-        <Typography 
-            variant='h5' 
-            component="div" 
-            gutterBottom
-        >
-            최다 조회 매뉴얼
-        </Typography>
+        <Box sx={{display : 'flex', justifyContent: 'space-between'}} >
+            <Typography 
+                variant='h5' 
+                component="div" 
+                gutterBottom
+            >
+                최다 조회 매뉴얼
+            </Typography>
+            <ThemeProvider theme={theme}>
+                <Button 
+                    variant="outlined"
+                    href="/create"
+                    sx={{mb : 1}}
+                >
+                    글쓰기
+                </Button>
+            </ThemeProvider>  
+        </Box>
         <Divider />
         <Table />
 
