362e0adfdb9294d5e4165861ff2e32887b75fd5c
KimSeonHui <zero9657@naver.com>
2022-02-08 16:00:41 +0900

    delete unneccessary thigs

--- Web-Project-react/client/src/component/SummernoteEditor.js
+++ Web-Project-react/client/src/component/SummernoteEditor.js
@@ -7,23 +7,23 @@ import 'react-summernote/lang/summernote-ko-KR'; // you can import any other loc
 import 'bootstrap/js/modal';
 import 'bootstrap/js/dropdown';
 import 'bootstrap/js/tooltip';
-//import 'bootstrap/dist/css/bootstrap.css';
-import '../summernoteStyle.scss'
+import 'bootstrap/dist/css/bootstrap.css';
+//import '../summernoteStyle.scss'
 
 
 function SummernoteEditor() {
-  const onChange = content => {
+  const onChange = (content) => {
     console.log("onChange ", content);
   };
 
     return (
       <div className='use-bootstrap'>
         <ReactSummernote
-          value="Default value"
+          value="내용을 입력해 주세요"
           options={{
             lang: 'ko-KR',
             height: 350,
-            dialogsInBody: false,
+            dialogsInBody: true,
             toolbar: [
               ['style', ['style']],
               ['font', ['bold', 'underline', 'clear']],
@@ -42,39 +42,3 @@ function SummernoteEditor() {
 }
 
 export default SummernoteEditor;
-
-// import React, { Component } from "react";
-// import ReactSummernote from "react-summernote";
-// import "react-summernote/dist/react-summernote.css"; // import styles
-
-// // Import bootstrap(v3 or v4) dependencies
-// import "bootstrap/js/modal";
-// import "bootstrap/js/dropdown";
-// import "bootstrap/js/tooltip";
-// import "bootstrap/dist/css/bootstrap.css";
-
-// export default class SummernoteEditor extends Component {
-//   onChange = (content) => {
-//     console.log("onChange", content);
-//   };
-//   render() {
-//     return (
-//       <ReactSummernote
-//         options={{
-//           disableDragAndDrop: true,
-//           height: 200,
-//           toolbar: [
-//             ["style", ["style"]],
-//             ["font", ["bold", "underline", "clear"]],
-//             ["fontname", ["fontname"]],
-//             ["para", ["ul", "ol", "paragraph"]],
-//             ["table", ["table"]],
-//             ["insert", ["link", "picture", "video"]],
-//             ["view", ["fullscreen", "codeview"]]
-//           ]
-//         }}
-//         onChange={this.onChange}
-//       />
-//     );
-//   }
-// }
