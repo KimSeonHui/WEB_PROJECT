456d91750ba7e2f33a76a594c8e3c1d63044f115
KimSeonHui <zero9657@naver.com>
2022-02-08 15:53:37 +0900

    create category select

--- Web-Project-react/client/src/component/CreatePage.js
+++ Web-Project-react/client/src/component/CreatePage.js
@@ -1,4 +1,6 @@
+import {useEffect, useState} from "react";
 import { Box, Typography, Divider, Container, Button, ButtonGroup } from '@mui/material';
+import { FormControl, Select, MenuItem  } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import SummernoteEditor from './SummernoteEditor';
 
@@ -11,6 +13,20 @@ const theme = createTheme({
   });
 
 function CreatePage() {
+    const [large, setLarge] = useState('');
+    const [medium, setMedium] = useState('');
+    const [small, setSmall] = useState('');
+
+    const handleLarge = (event) => {
+        setLarge(event.target.value);
+    }
+    const handleMedium = (event) => {
+        setMedium(event.target.value);
+    }
+    const handleSmall = (event) => {
+        setSmall(event.target.value);
+    }
+
     return <Container maxWidth="xl">
     <Box sx={{width : "100%", p : '20px'}}>
     <Box sx={{display : 'flex', justifyContent: 'space-between'}} >
@@ -29,7 +45,29 @@ function CreatePage() {
         </ThemeProvider>  
     </Box>
    
-    <Divider />
+    <Divider sx={{mb : 2}} />
+    <FormControl sx={{mb : 2, width: 1/3}}>
+        <Select id='large' value={large} onChange={handleLarge} sx={{ mr: 1}} displayEmpty>
+            <MenuItem value="">
+                <em>카테고리 1 선택</em>
+            </MenuItem>
+            <MenuItem value="1">one</MenuItem>
+        </Select>
+    </FormControl>
+    <FormControl sx={{mb : 2, width: 1/3}}>
+        <Select id='large' value={medium} onChange={handleMedium} sx={{ mx: 1}} displayEmpty>
+            <MenuItem value="">
+                <em>카테고리 2 선택</em>
+            </MenuItem>
+        </Select>
+    </FormControl>
+    <FormControl sx={{mb : 2, width: 1/3}}>
+        <Select id='large' value={small} onChange={handleSmall} sx={{ ml: 1}} displayEmpty>
+            <MenuItem value="">
+                <em>카테고리 3 선택</em>
+            </MenuItem>
+        </Select>
+    </FormControl>
     <SummernoteEditor />
     </Box>
 </Container>
