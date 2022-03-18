0d9186501c5b66362a012d9e7699c6764e30935c
KimSeonHui <zero9657@naver.com>
2022-03-18 19:51:02 +0900

    change page when click paginavigation page

--- Web-Project-react/client/src/component/Paginavigation.js
+++ Web-Project-react/client/src/component/Paginavigation.js
@@ -1,7 +1,6 @@
-import { Box, Pagination } from '@mui/material';
-import axios from "axios";
+import { Box, Pagination, PaginationItem, Link } from '@mui/material';
 
-function Paginavigation() {
+function Paginavigation({ cid, page }) {
 
     return <Box sx={{display : 'flex', justifyContent : 'center', mt: 6 }}>
         <Pagination 
@@ -13,6 +12,14 @@ function Paginavigation() {
             showFirstButton 
             showLastButton
             boundaryCount={5}
+            defaultPage={parseInt(page)}
+            renderItem={(item) => (
+                <PaginationItem
+                  component={Link}
+                  href={`/board/${cid}/${item.page}`}
+                  {...item}
+                />
+            )}
         />
     </Box>
 }
