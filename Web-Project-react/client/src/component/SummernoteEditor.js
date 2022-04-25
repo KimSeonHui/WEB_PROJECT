c62b8bbeccfa45f285ede3a41491469b57ea2356
KimSeonHui <zero9657@naver.com>
2022-04-25 15:03:09 +0900

    add image upload logic

--- Web-Project-react/client/src/component/SummernoteEditor.js
+++ Web-Project-react/client/src/component/SummernoteEditor.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactSummernote from 'react-summernote';
 import 'react-summernote/dist/react-summernote.css'; // import styles
 import 'react-summernote/lang/summernote-ko-KR'; // you can import any other locale
+import axios from 'axios';
 
 // Import bootstrap(v3 or v4) dependencies
 import 'bootstrap/js/modal';
@@ -15,6 +16,38 @@ function SummernoteEditor({setDesc}) {
   const onChange = (content) => {
     setDesc(content);
   };
+
+  const onImageUpload = async (files) => {   
+    for(let i = 0; i < files.length; i++) {
+      const reader = new FileReader();
+      
+      reader.onloadend = () => {
+        ReactSummernote.insertImage(reader.result);
+      }
+      reader.readAsDataURL(files[i]);
+    }
+    
+    //FormData() 사용해 서버에 저장하는 방법 - 서버에 저장한 url로 이미지 불러올 수가 x
+    // const data = new FormData();
+    // data.append('file', fileList[0]);
+
+    // const res = await axios.post('/upload/images', data, {
+    //   headers: {
+    //     'Content-Type': 'multipart/form-data'
+    //   }
+    // });
+
+    // if(res.statusText === 'OK') {
+    //   console.log('res.data', res.data.url);
+
+    //   if(res.data === 'error') {
+    //     alert('오류가 발생 했습니다.');
+    //   }
+    //   else {
+    //     ReactSummernote.insertImage(`../../server/public/${res.data.url}`);
+    //   }
+    // }
+  }
 
     return (
       <div className='use-bootstrap'>
@@ -35,6 +68,7 @@ function SummernoteEditor({setDesc}) {
             ]
           }}
           onChange={onChange}
+          onImageUpload={onImageUpload}
         />
       </div>  
       );
